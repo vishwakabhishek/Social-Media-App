e1c3c1ada7321bff7df0530d680e777f71caac90
vishwakabhishek <vishwakabhishek@gmail.com>
2023-05-29 18:40:13 +0530

    First commit of my social media app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './App.css'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App ">
+      <div className='app__header'>
+        <img className='app__headerImage'
+        src="https://www.bananaip.com/wp-content/uploads/2016/11/Instagram_logo.svg_.png"
+        alt="Instagram logo"/>
+      </div>
     </div>
   );
 }
